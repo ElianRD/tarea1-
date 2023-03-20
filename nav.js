a204d45a8c1b71171b72d52e6bb7cccb87593d3c
ElianRD <elianencarnacion03@gmail.com>
2023-03-20 16:10:27 -0400

    ultimo comit

--- nav.js
+++ nav.js
@@ -2,3 +2,15 @@ function openNav() {
   const menu = document.getElementById("overlay1");
   menu.classList.toggle("cierre");
 }
+
+const intervalID = setInterval(myCallback, 10000);
+
+function myCallback() {
+  const $img1 = document.getElementById("yoo");
+  $img1.src =
+    "./img/Captura_de_pantalla_2023-03-15_122726-removebg-preview.png";
+  setTimeout(function () {
+    $img1.src = "./img/1677889939897-removebg-preview.png";
+    // Aquí va la función que deseas ejecutar
+  }, 5000);
+}
